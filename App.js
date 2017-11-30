1fbcaf068cbd7a00828c6c0f5127e77fb3367fe6
deston-trace <destontrace@gmail.com>
2017-11-30 18:07:25 +0500

    full application

---
+++ App.js
@@ -0,0 +1,19 @@
+// appliaction code placeholder
+
+import React from 'react';
+import { View } from 'react-native';
+import Header from './src/components/header';
+import AlbumList from './src/components/AlbumList';
+
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={{ flex: 1 }}>
+        <Header headerText={'album!'} />
+        <AlbumList />
+      </View>
+
+    );
+  }
+}
